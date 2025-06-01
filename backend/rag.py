e3059db18bab3f2090771c0571256ba9adfe6214
hbob <119982677+hparupudi@users.noreply.github.com>
2025-06-01 11:50:30 -0400

    Update rag.py

--- backend/rag.py
+++ backend/rag.py
@@ -1,13 +1,21 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Any
 from dotenv import load_dotenv
 from langchain.output_parsers import PydanticOutputParser
+from langchain_text_splitters import RecursiveJsonSplitter
 from pypdf import PdfReader
+from langchain.docstore.document import Document
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
 import re
+import json
+import uuid
 
 load_dotenv()
 
@@ -32,9 +40,10 @@ prompt = PromptTemplate(
 )
 
 llm = ChatOpenAI(model="gpt-4o", api_key=os.getenv('OpenAI_API'), temperature=1.0)
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=os.getenv('OpenAI_API'))
 
 #Function to combine question & answer into one object and make a list of those dict objects
-def generate_test_dict(question_url, answer_url):
+def generate_test_list(question_url, answer_url):
 
   question_reader = PdfReader(question_url)
   question_text = ""
@@ -52,22 +61,35 @@ def generate_test_dict(question_url, answer_url):
     input={"test": f"{question_text}", "answer_key": f"{answer_text}"}
   )
 
-  question_bank = []
+  question_list = []
   for x in range(len(response['text'].questions)):
-    question_object = {
-        'question': response['text'].questions[x],
-        'answer': response['text'].answers[x]
-    }
-    question_bank.append(question_object)
+    question_str = "Question: " + response['text'].questions[x] + " Answer: " + response['text'].answers[x]
+    question_list.append(question_str)
 
-  return question_bank
+  return question_list
 
+def load_question_bank(question_list, index):
+    docs = []
+    for q in question_list:
+        doc = Document(page_content=q, metadata={"source": "local"}, id=str(uuid.uuid4()))
+        docs.append(doc)
+    vectorstore = PineconeVectorStore(index_name=index, embedding=embeddings, pinecone_api_key=os.getenv('PINECONE_API'))
+    vectorstore.add_documents(documents=docs)
+
+index = "sciolyastronomy"
 astro_path = "C:\\Users\\harsh\\scioly_backend\\SciOly Past Tests\\Astronomy"
 astro_folders = list(os.scandir(astro_path))
 astro_keys = list(os.scandir(astro_folders[0].path))
-astro_tests = list(os.scandir(astro_folders[1].path))
+astro_test_images = list(os.scandir(astro_folders[1].path))
 
-question_pdf = str(astro_tests[0].path)
-answer_pdf = str(astro_keys[0].path)
-question_bank = generate_test_dict(question_pdf, answer_pdf)
-print(question_bank)
+for x in range(len(astro_keys)):
+    answer_pdf = str(astro_keys[x].path)
+    question_dir = list(os.scandir(astro_test_images[x].path))
+    if len(question_dir) > 1:
+        question_pdf = question_dir[1].path
+    else:
+       question_pdf = question_dir[0].path
+    question_list = generate_test_list(question_pdf, answer_pdf)
+    load_question_bank(question_list, index)
+
+print("Completed!")
