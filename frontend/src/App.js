d362177b641174fced4e21c386095d97d16c2dd0
hbob <119982677+hparupudi@users.noreply.github.com>
2025-06-03 22:13:43 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import LandingPage from './components/LandingPage.js'
-import Aristocrats from './components/Aristocrats.js'
+import LandingPage from './components/LandingPage.js';
+import Aristocrats from './components/Aristocrats.js';
+import MenuPage from './components/MenuPage.js';
+import MCQPage from './components/MCQPage.js';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
-        <Route path="/code" element={<Aristocrats/>}/>
+        <Route path="/aristo" element={<Aristocrats/>}/>
+        <Route path="/menu" element={<MenuPage/>}/>
+        <Route path="/mcq" element={<MCQPage/>}/>
       </Routes>
     </Router>
   );
